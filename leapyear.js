24b809043816d746ff8374339285ebfdef8ce9c9
sagorcodes <shossen.cms@gmail.com>
2023-08-02 17:03:17 +0300

    create leapyear.js file

---
+++ leapyear.js
@@ -0,0 +1,14 @@
+//  leapYear() নামে ফাংশন লিখো এবং নেক্সট ইয়ার অর্থাৎ ২০২৩ কি leap year নাকি সেটা চেক করো। Leap year হলে ফাংশন true রিটার্ন করবে আর leap year না হলে false রিটার্ন করবে।
+
+function leapYear(year) {
+    if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// Example usage:
+let yearToCheck = 2023;
+let isLeap = leapYear(yearToCheck);
+console.log(isLeap);  // Output will be false
