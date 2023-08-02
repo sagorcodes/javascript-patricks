03e714f6e034be532b3446948073d292b7481b50
sagorcodes <shossen.cms@gmail.com>
2023-08-02 21:42:43 +0300

    create js file for miles To Kilometer convert

---
+++ miles-to-km.js
@@ -0,0 +1,9 @@
+// miles To Kilometer convert
+
+function milesToKilometer(miles){
+    let km = miles*1.609344;
+    return km;
+}
+let km = 1;
+let result = milesToKilometer (km);
+console.log(result);
