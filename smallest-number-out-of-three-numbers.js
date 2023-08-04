5c9901252890b5279f8fbe32f67c1d1650e83bc8
sagorcodes <shossen.cms@gmail.com>
2023-08-04 17:41:32 +0300

    find the smallest number out of three numbers

---
+++ smallest-number-out-of-three-numbers.js
@@ -0,0 +1,18 @@
+function findSmallestNumber(a, b, c) {
+    if (a <= b && a <= c) {
+      return a;
+    } else if (b <= a && b <= c) {
+      return b;
+    } else {
+      return c;
+    }
+  }
+  
+  // Example usage:
+  const num1 = 8;
+  const num2 = 4;
+  const num3 = 11;
+  
+  const smallest = findSmallestNumber(num1, num2, num3);
+  console.log("The smallest number is:", smallest);
+  
