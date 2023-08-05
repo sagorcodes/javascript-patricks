8f387d2dd772fabbc052a95c7a0e135cd9f7405e
sagorcodes <shossen.cms@gmail.com>
2023-08-05 19:51:24 +0300

    Will change the value of one

---
+++ change-the-value.js
@@ -0,0 +1,8 @@
+// Using 'let' for a variable that can be changed
+let count = 5;
+count = 10; // Changing the value
+
+// Using 'const' for a variable that should not be changed
+const maxAttempts = 3;
+// You can't change the value of 'maxAttempts'
+// maxAttempts = 5; // This would result in an error
