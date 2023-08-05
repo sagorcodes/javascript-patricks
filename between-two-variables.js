ca83fcffbc4205f68cad0552c8176690c8ee168e
sagorcodes <shossen.cms@gmail.com>
2023-08-05 20:16:35 +0300

    Subtract, multiply, divide and find quotients between two variables

---
+++ between-two-variables.js
@@ -0,0 +1,19 @@
+// Can you compare two variables? You can compare. Of the two variables, the first is less than, greater than, unequal to, equal to, less than or equal to, greater than or equal to the second. You can check these. That is, you can use <, >, ==, !=, <=, >= symbols. Then declare two number type variables and compare them in six ways
+
+
+let num1 = 7;
+let num2 = 3;
+const sum = num1 + num2;
+console.log("Total", sum);
+
+const sum1 = num1 - num2;
+console.log("Total", sum1)
+
+const sum2 = num1 * num2;
+console.log("Total", sum2)
+
+const sum3 = num1 / num2;
+console.log("Total", sum3)
+
+const sum4 = num1 % num2;
+console.log("Total", sum4)
