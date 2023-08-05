9a89abc9b99a7752cab18fc2680f4c8b00519176
sagorcodes <shossen.cms@gmail.com>
2023-08-06 01:46:15 +0300

    It will have three properties. An object changes the value of a property.

---
+++ object.js
@@ -0,0 +1,15 @@
+// Declare an object. It will have three properties. And change the value of a property.
+
+// Declare an object with three properties
+var person = {
+    firstName: "Jhankar",
+    lastName: "Mahbub",
+    age: 30
+  };
+  
+  // Change the value of a property
+  person.age = 35;
+  
+  // Output the updated object
+  console.log("Updated person object:", person);
+  
