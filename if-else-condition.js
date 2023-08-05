084fff37bf93a3be705e85fd669cacf5dc5b4b1b
sagorcodes <shossen.cms@gmail.com>
2023-08-05 21:42:14 +0300

    how the if-else construct works

---
+++ if-else-condition.js
@@ -0,0 +1,9 @@
+// Will you fulfill a condition? Something else will do to not meet the condition. What words do you use in the idyll of if-else? Drop a virus
+
+let temperature = 25;
+
+if (temperature > 30) {
+  console.log("It's a hot day!");
+} else {
+  console.log("It's not too hot today.");
+}
