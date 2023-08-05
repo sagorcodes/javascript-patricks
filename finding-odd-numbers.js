21a9b2703aab882a37192fd991509b4ef0dec695
sagorcodes <shossen.cms@gmail.com>
2023-08-05 21:58:46 +0300

    finding odd numbers

---
+++ finding-odd-numbers.js
@@ -0,0 +1,8 @@
+// If you can say then loop through all the odd numbers from 7 to 19. Will you see you? Leave that effect.
+
+for (var i = 7; i <= 19; i++) {
+    if (i % 2 !== 0) {
+      console.log(i);
+    }
+  }
+  
